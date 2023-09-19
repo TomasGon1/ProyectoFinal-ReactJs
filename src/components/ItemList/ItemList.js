16692a8ef51bdefb265fc9722d7c32bb54056805
TomasGon1 <xnicksyux@gmail.com>
2023-09-19 20:08:53 -0300

    proyecto-ecomm

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import Item from "../Item/Item"
+import React from "react";
+import Item from "../Item/Item";
 
-const ItemList = ({item}) => {
+const ItemList = ({ item }) => {
   return (
-    <div className='row' id='itemList'>
-      {item.map(i =>
-        <div className='col-md-3' key={i.id}>
-          <Item items={item}/>
-          </div>
-        )}
-      
+    <div className="row" id="itemList">
+      {item.map((i) => (
+        <div className="col-md-3" key={i.id}>
+          <Item items={item} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ItemList
+export default ItemList;
