393cbd78a5ae39801b3f0b9640c8052237a60184
TomasGon1 <xnicksyux@gmail.com>
2023-10-04 21:29:29 -0300

    proyecto-ecomm

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Error404.css";
 
 const Error404 = () => {
   return (
     <div>
-      <Link to={"*"}>
-        <h2>Error 404</h2>
-        <p>Imposible cargar...</p>
+      <h2 className="d-flex justify-content-center h2-style">Error 404</h2>
+      <p className="d-flex justify-content-center p-style">
+        Imposible cargar... 😞😞
+      </p>
+
+      <Link to={"/"} className="d-flex justify-content-center h3-style">
+        Volver al inicio
       </Link>
     </div>
   );
